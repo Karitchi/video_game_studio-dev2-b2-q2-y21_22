1050361ab234dd5000f4bca4ba6510a523495bec
Nathan-Sancke <73235503+Nathan-Sancke@users.noreply.github.com>
2022-03-28 12:10:45 +0200

    Career: better caroussel

--- Web-App/assets/components/Caroussel.js
+++ Web-App/assets/components/Caroussel.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import Carousel from 'react-elastic-carousel';
 import Card from "./Card";
 
-const handleDragStart = (e) => e.preventDefault();
-
-const items = [
-    <Card onDragStart={handleDragStart} title={"Job Title 1"} location={"Brussels"} description={"Quisque euismod, dolor a rhoncus placerat, nunc libero ultrices tortor, quis dapibus urna tortor ut nunc. Nunc faucibus ex vel nibh feugiat, ut venenatis nisl sodales."}/>,
-    <Card onDragStart={handleDragStart} title={"Job Title 2"} location={"Brussels"} description={"Quisque euismod, dolor a rhoncus placerat, nunc libero ultrices tortor, quis dapibus urna tortor ut nunc. Nunc faucibus ex vel nibh feugiat, ut venenatis nisl sodales."}/>,
-    <Card onDragStart={handleDragStart} title={"Job Title 3"} location={"Brussels"} description={"Quisque euismod, dolor a rhoncus placerat, nunc libero ultrices tortor, quis dapibus urna tortor ut nunc. Nunc faucibus ex vel nibh feugiat, ut venenatis nisl sodales."}/>,
-    <Card onDragStart={handleDragStart} title={"Job Title 4"} location={"Brussels"} description={"Quisque euismod, dolor a rhoncus placerat, nunc libero ultrices tortor, quis dapibus urna tortor ut nunc. Nunc faucibus ex vel nibh feugiat, ut venenatis nisl sodales."}/>,
-
-];
 
 function Caroussel() {
+
+    const breakPoints = [
+        {width: 1, itemsToShow: 1},
+        {width: 500, itemsToShow: 2},
+        {width: 1000, itemsToShow: 3},
+        {width: 1500, itemsToShow: 4},
+        {width: 2000, itemsToShow: 5}
+
+    ]
+
     return (
         <div>
             <h1>Current job Openings:</h1>
-            <AliceCarousel mouseTracking items={items} />
+            <Carousel breakPoints={breakPoints}>
+                <Card title="Job Title 1" location="Brussels" description="Quisque euismod, dolor a rhoncus placerat, nunc libero ultrices tortor, quis dapibus urna tortor ut nunc. Nunc faucibus ex vel nibh feugiat, ut venenatis nisl sodales."/>
+                <Card title="Job Title 2" location="Brussels" description="Quisque euismod, dolor a rhoncus placerat, nunc libero ultrices tortor, quis dapibus urna tortor ut nunc. Nunc faucibus ex vel nibh feugiat, ut venenatis nisl sodales."/>
+                <Card title="Job Title 3" location="Brussels" description="Quisque euismod, dolor a rhoncus placerat, nunc libero ultrices tortor, quis dapibus urna tortor ut nunc. Nunc faucibus ex vel nibh feugiat, ut venenatis nisl sodales."/>
+                <Card title="Job Title 4" location="Brussels" description="Quisque euismod, dolor a rhoncus placerat, nunc libero ultrices tortor, quis dapibus urna tortor ut nunc. Nunc faucibus ex vel nibh feugiat, ut venenatis nisl sodales."/>
+            </Carousel>
         </div>
-
     );
 }
 
