413db62a7ba7cf4a6e968a33bb94f242fc2d3c68
MarkBorca <60783220+MarkBorca@users.noreply.github.com>
2022-03-28 19:06:07 +0200

    mongodb library
    doctrine is a symfony dependendency

---
+++ Web-App/assets/components/MainTextBox.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import style from './style/MainTextBox.css'
+
+const MainTextBox = () => {
+    return (
+        <div className="main-text-box">
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+            <p>
+                Vivamus nec sapien varius, pretium tortor eget, vestibulum purus. Donec scelerisque ut tortor et egestas.
+                Fusce id porttitor velit. Suspendisse accumsan, velit sed pellentesque varius, est enim suscipit lectus,
+                quis varius ipsum erat ut nulla. Ut eu tincidunt enim, et aliquam mauris. Pellentesque habitant morbi
+                tristique senectus et netus et malesuada fames ac turpis egestas. Sed rutrum arcu pellentesque suscipit
+                convallis.
+            </p>
+        </div>
+    )
+}
+
+export default MainTextBox
