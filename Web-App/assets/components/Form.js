5364ca04d0e20745164d077777097b2a56d289ec
MarkBorca <60783220+MarkBorca@users.noreply.github.com>
2022-04-12 00:20:42 +0200

    helped with grid display
    todo:
    - css font size acts weird
    - css define style on image block
    - make form size responsive
    - make links to facebook, twitter, steam as links leading to '/'

--- Web-App/assets/components/Form.js
+++ Web-App/assets/components/Form.js
@@ -7,39 +7,45 @@ import Toggle from '../components/style/Toggle.css'
 const Form = () => {
     return (
         <div className='div-container'>
-            <div>
+            <div className="form-container">
                 <div className='div-child toggle-button'>
-                    <input type='checkbox' id='toggle' class='toggleCheckbox' value='report' />
-                    <label for='toggle' className='toggleContainer'>
-                        <div>Report Bug</div> 
+                    <input type='checkbox' id='toggle' className='toggleCheckbox' value='report' />
+                    <label htmlFor='toggle' className='toggleContainer'>
+                        <div>Report Bug</div>
                         <div>General Form</div>
                     </label>
                 </div>
 
                 <div className='div-child formular'>
                     <form>
-                        <label for="fname">Name</label><br></br>
-                        <input name='Name' className='name' type="text" id="fname" required='true' ></input><br></br>
-                        <label for="fmail">Email*</label><br></br>
-                        <input name='Mail' className='mail' type="email" id="fmail" required='true' ></input><br></br>
-                        <label>Game*</label><br></br>
+                        <label htmlFor="fname">Name</label>
+                        <input name='Name' className='name' type="text" id="fname" required='true' >
+
+                        </input>
+                        <label htmlFor="fmail">Email*</label>
+                        <input name='Mail' className='mail' type="email" id="fmail" required='true' >
+
+                        </input>
+                        <label>Game*</label>
                         <select name='Game' className='game'>
                             <option value='game1'>Game1</option>
                             <option value='game2'>Game2</option>
                             <option value='game3'>Game3</option>
-                        </select><br></br>
-                        <label>Category*</label><br></br>
-                        <select name='Categ' className='category'><br></br>
+                        </select>
+                        <label>Category*</label>
+                        <select name='Categ' className='category'>
                             <option value='categ1'>Categ1</option>
                             <option value='categ2'>Categ2</option>
                             <option value='categ3'>Categ3</option>
-                        </select><br></br>
+                        </select>
                         <label>Description</label>
-                        <textarea name='Msg' className='description' cols='5' rows='5' placeholder='Write your message here...'></textarea>
+                        <textarea name='Msg' className='description' cols='5' rows='5' placeholder='Write your message here...'>
+
+                        </textarea>
                         <button type='submit'>Submit</button>
                     </form>
-            </div>
- 
+                </div>
+
             </div>
             <div className='div-child media'>
                 <strong>Connect with us on Social Media</strong>
@@ -49,7 +55,7 @@ const Form = () => {
                 <p>Connect on Steam</p>
             </div>
         </div>
-        
+
     )
 }
 
