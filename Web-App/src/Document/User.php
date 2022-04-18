1af30d7194a331d54ddcba3a051f71eb83945efc
MarkBorca <60783220+MarkBorca@users.noreply.github.com>
2022-04-18 12:58:24 +0200

    Successful connection to db
    connection to db and simple output of query data
    
    Co-Authored-By: NechiforBogdan <71372147+NechiforBogdan@users.noreply.github.com>

---
+++ Web-App/src/Document/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document
+ */
+class User
+{
+    /**
+     * @MongoDB\Id
+     */
+    protected $_id;
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $email;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $password;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $role;
+}
