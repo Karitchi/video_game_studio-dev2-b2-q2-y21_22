645cfb639573b71dd9c6fbe62035f01605465928
Nathan-Sancke <73235503+Nathan-Sancke@users.noreply.github.com>
2022-04-22 21:05:30 +0200

    About: first draw game

---
+++ Web-App/assets/components/Silva_bird.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style/Silva_bird.css'
+
+function Silva_bird() {
+    return(
+        <div className='silva_bird'>
+            <iframe src="https://idyllic-salamander-605862.netlify.app" width="577" height="500" scrolling="no" />
+        </div>
+    )
+}
+
+export default Silva_bird
