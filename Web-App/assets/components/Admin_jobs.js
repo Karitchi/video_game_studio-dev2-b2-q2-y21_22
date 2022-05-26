ecf63f0fe81cd8b765dc58412d236630e675f7ad
Nathan-Sancke <73235503+Nathan-Sancke@users.noreply.github.com>
2022-05-26 15:28:06 +0200

    admin: bug fix

--- Web-App/assets/components/Admin_jobs.js
+++ Web-App/assets/components/Admin_jobs.js
@@ -3,34 +3,37 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './style/Admin_jobs.css'
 import Card_admin from "./Card_admin";
 import $ from "jquery";
+import Carousel from "react-bootstrap/Carousel";
+import Card from "./Card";
 
 const array1 = [];
-const array2 = [];
+const array4 = [];
 
 function Admin_jobs(props) {
     useEffect(()=>{jsonParser()},[])
     const [listJobs, setListJobs] = useState([])
     const [listCompany, setListCompany] = useState([])
-    const [alacon, setAlacon] = useState('lul')
-    let temp = ''
+    let temp = []
 
     useEffect(()=>{
         $.getJSON('/api/jobs', data=>{
             let myData = JSON.parse(data);
+            setListJobs(myData)
             for (let i of myData) {
                 array1.push(i);
             }
-            for (let i=0; i<array1.length; i++) {
-                temp = temp +("<Card_admin title={listJobs["+i+"].job_title} location={listJobs["+i+"].location} skills={listJobs["+i+"].skills_and_experience} contact={'email'} description={listJobs["+i+"].job_description}tasks={listJobs["+i+"].job_description}/>")
-                // console.log(array1[i])
-                // console.log(temp)
-                // console.log(i.location)
 
-            }
+            })
+        },[])
 
-            // setAlacon(temp)
-            },[])
-        })
+    useEffect(() =>{
+        for (let i=0; i<listJobs.length; i++) {
+            temp.push(<Card_admin key={`${listJobs[i].job_title} - ${i}`} title={listJobs[i].job_title} location={listJobs[i].location} skills={listJobs[i].skills_and_experience} contact='email' description={listJobs[i].job_description} tasks={listJobs[i].tasks}/>)
+            console.log(typeof (temp))
+        }
+        setListCompany(temp)
+    },[listJobs])
+
 
 
 
@@ -38,9 +41,7 @@ function Admin_jobs(props) {
         <div className='content_admin_jobs'>
             <h2>Careers:</h2>
             <div className="gallery">
-                {/*{setListJobs(array1)}*/}
-                {/*{temp}*/}
-                {/*{alacon}*/}
+                {listCompany}
             </div>
         </div>
     )
@@ -53,7 +54,7 @@ function jsonParser(){
     $.getJSON('/api/company_data', data=>{
         let myData = JSON.parse(data);
         for (let i of myData) {
-            array2.push(i);
+            array4.push(i);
         }
     })
 }
